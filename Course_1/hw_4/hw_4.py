148413c48a32720918069baaa8bbffb1ecef4b5f
MarinaAlPu <marina.pudovkina13@gmail.com>
2023-05-16 13:48:03 +0300

    commented out supporting prints

--- Course_1/hw_4/hw_4.py
+++ Course_1/hw_4/hw_4.py
@@ -1,5 +1,6 @@
-# Переменные, константы
+# Константы, переменные
 WORDS = 5
+LEVELS = ["легкий", "средний", "сложный"]
 
 levels_for_select = {
     "easy": "легкий",
@@ -40,51 +41,60 @@ levels = {
     5: "Отлично",
 }
 
+words_all = {
+    "easy": words_easy,
+    "medium": words_medium,
+    "hard": words_hard,
+}
+
 words = {}
 answers = {}
 right_answered_words = []
 wrong_answered_words = []
 
-user_input = None # или user_input = ""
-edited_user_input = None # или user_input = ""
+# user_try = 2
+user_input = None
+edited_user_input = None
 letter_unneeded = "ё"
 letter_needed = "е"
 dict_name = "words"
-correct = "Правильно отвечены слова: "
-incorrect = "Неправильно отвечены слова: "
+correct = "Правильно отвечены слова:"
+incorrect = "Неправильно отвечены слова:"
 amount_right_answered_words = 0
 
-# words = words_easy
-# print(words_easy)
-# print(words)
+
+levels_for_intro = ", ".join(LEVELS)
+
+print(f"Выберите уровень сложности: {levels_for_intro}.\n")
 
 # Получить у пользователя уровень сложности
-print("Выберите уровень сложности: легкий, средний, сложный.\n")
 user_input = input().lower()
 
+# Проверить введённое значение на наличие буквы "ё" и заменить её на букву "е"
 if letter_unneeded in user_input:
     edited_user_input = user_input.replace(letter_unneeded, letter_needed)
+else:
+    edited_user_input = user_input
+
 
 # Положить в словарь words один из словарей в зависимости полученного от пользователя уровня сложности
 for level_en, level_ru in levels_for_select.items():
-        if edited_user_input == level_ru:
-            words = (f"{dict_name}_{level_en}")
-            print(f"\nСловарь с учётом уровня сложности:\n{words}")
-            print(f"Тут я хочу напечатать весь словарь {words}")
-            print(words) # а словарь не печатается, печатается только название словаря, потому что words - это str
-            break
-        else:
-            pass
-            # user_input = input("\nТакого уровня не существует.\nВыберите уровень сложности из следующих: лёгкий, средний, сложный.\n").lower()
+    if edited_user_input == level_ru:
+        words = words_all[level_en]
+        print(words)
+        break
+    else:
+        pass
+        # user_input = input(f"\nТакого уровня не существует.\nВыберите уровень сложности из следующих: {levels_for_intro}.\n").lower()
 
 
-user_input = input(f"\nВыбран уровень сложности {level_ru}, мы предложим {WORDS} слов, подберите перевод.\nНажмите Enter.")
+user_input = input(f"\nВыбран уровень сложности {edited_user_input}, мы предложим {WORDS} слов, подберите перевод.\nНажмите Enter.")
 
 
 # Запустить цикл по пяти словам из словаря words
-for word, translation in words_easy.items(): # заменить words_easy на words когда разберусь как в words положить словарь words_easy
+for word, translation in words.items():
 # Получить у пользователя ответ
-    user_input = input(f"{word}, {len(word)} букв, начинается на {translation[0]}: ")
+    user_input = input(f"{word}, {len(translation)} букв, начинается на {translation[0]}: ")
     print(user_input)
     if user_input.lower() == translation:
 # Записать результат в answers
@@ -94,27 +104,26 @@ for word, translation in words_easy.items(): # заменить words_easy на 
         answers[word] = False
         print(f"Неверно, {word.title()} — это {translation}.")
 
-print("\nПечатаю словарь с ответами")
-print(answers)
+
+print(f"\nПечатаю словарь с ответами {answers}")
 
 
-# Когда слова закончились, вывести в зависимости от результата правильно и неправильно отвеченные слова
+# Когда слова закончились, вывести правильно и неправильно отвеченные слова
 for key, value in answers.items():
         if value == True:
             right_answered_words.append(key)
         else:
             wrong_answered_words.append(key)
 
-print(f"\n{correct}:")#{right_answered_words}
-# print(right_answered_words)
 
+# Список правильно отвеченных слов
+print(f"\n{correct}:")
 for i in range(len(right_answered_words)):
     print(right_answered_words[i])
 
 
-print(f"\n{incorrect}:")#{wrong_answered_words}
-# print(wrong_answered_words)
-
+# Список неправильно отвеченных слов
+print(f"\n{incorrect}:")
 for i in range(len(wrong_answered_words)):
     print(wrong_answered_words[i])
 
@@ -124,11 +133,12 @@ for value in answers.values():
     if value == True:
         amount_right_answered_words += 1
 
-# amount_right_answered_words = len(right_answered_words)
-# amount_right_answered_words = len(list(answers.keys()))
+amount_right_answered_words = len(right_answered_words)
+amount_right_answered_words = len(list(answers.keys()))
 # print(amount_right_answered_words)
+
 
 # Вывеcти ранг пользователя, используя в качестве ключа количество правильно отгаданных слов
 for key, level in levels.items():
     if key == amount_right_answered_words:
-        print(f"Ваш ранг: {level}.")
+        print(f"\nВаш ранг: {level}.")
