6742ec573895615d68c5a7b017afd3d2530ecb42
MarinaAlPu <marina.pudovkina13@gmail.com>
2023-06-18 22:54:33 +0500

    corrected for checking (does not work)

--- Course_2/hw_7/utils.py
+++ Course_2/hw_7/utils.py
@@ -1,115 +1,60 @@
-import json
-
-
-def load_students():
-    """
-    Загружает список студентов из файла
-    """
-    with open('students.json') as students:
-        students_data = students.read()
-        students_data_list = json.loads(students_data)
-        students_list = []
-        for i in range(len(students_data_list)):
-            students_list.append(students_data_list[i]["pk"])
-        return students_data_list, students_list
-
-
-def load_professions():
-    """
-    Загружает список профессий из файла
-    """
-    with open('professions.json') as professions:
-        professions_data = professions.read()
-        professions_data_list = json.loads(professions_data)
-        professions_list = []
-        for i in range(len(professions_data_list)):
-            professions_list.append(professions_data_list[i]["title"])
-        return professions_data_list, professions_list
-
-
-def get_student_by_pk(pk):
-    """
-    Получает словарь с данными студента по его pk
-    """
-    with open('students.json') as students:
-        students_data = students.read()
-        students_data_list = json.loads(students_data)
-        for i in range(len(students_data_list)):
-            if pk == students_data_list[i]["pk"]:
-                return students_data_list[i]
-
-
-def get_profession_by_title(title):
-    """
-    Получает словарь с информацией о профессии по названию
-    """
-    with open('professions.json') as professions:
-        professions_data = professions.read()
-        professions_data_list = json.loads(professions_data)
-        for i in range(len(professions_data_list)):
-            if title.title() == professions_data_list[i]["title"]:
-                return professions_data_list[i]
-
-
-def check_fitness(student, profession):
-    """
-    Получив студента и профессию, возвращала бы словарь типа:
-    {
-    "has": ["Python", "Linux"], # есть у студента
-    "lacks": ["Docker, SQL"], # не хватает у студента
-    "fit_percent": 50 # пригодность студента
-    }
-    """
-    student_data_dict = get_student_by_pk(student)
-    student_skills_list = student_data_dict["skills"]
-    student_skills = set(student_skills_list)
-
-    profession_data_dict = get_profession_by_title(profession)
-    profession_skills_list = profession_data_dict["skills"]
-    profession_skills = set(profession_skills_list)
-
-    skills_to_check_fitness = student_skills.intersection(profession_skills)
-    skills_to_check_fitness_list = list(skills_to_check_fitness)
-
-    profession_skills_quantity = len(profession_skills_list)
-    skills_to_check_fitness_quantity = len(skills_to_check_fitness_list)
-    fit_percent_int = int((skills_to_check_fitness_quantity / profession_skills_quantity) * 100)
-    fit_percent = (f"{fit_percent_int}%")
-
-    student_skills_lacks = list(profession_skills.difference(student_skills))
-
-    check_fitness_dict = {}
-    check_fitness_dict["has"] = student_skills_list
-    check_fitness_dict["lacks"] = student_skills_lacks
-    check_fitness_dict["fit_percent"] = fit_percent
-
-    return check_fitness_dict
+from functions import *
 
 
 # Получить ввод номера студентао от пользователя
-student_input = input("Введите номер студента\n")
+student_input = int(input("Введите номер студента\n"))
 
 # Проверить существование такого пользователя
-students_data_list, students_list = load_students()
-if int(student_input) in students_list:
-    student_info = get_student_by_pk(int(student_input))
-    student_name = student_info['full_name']
-else:
-    print("У нас нет такого студента")
-    quit()
+students_list = load_students(DATA_SOURCE_STUDENTS)
+print(students_list, '\n', type(students_list))
+
+for i in range(len(students_list)):
+    if students_list[i]["pk"] == student_input:
+        student_info = get_student_by_pk(student_input, DATA_SOURCE_STUDENTS)
+        student_name = student_info["full_name"]
+    # else:
+    #     print("У нас нет такого студента")
+    #     quit()
+
+#
+# for student in students_list:
+#     if student_input in student["pk"]:
+#         student_info = get_student_by_pk(student_input, DATA_SOURCE_STUDENTS)
+#         print(student_info)
+#         student_name = student_info["full_name"]
+#         # break
+#     else:
+#         print(f"если нет студента: student_input, type(student_input)")
+#         print("У нас нет такого студента")
+#         quit()
 
 
 # Получить ввод названия профессии от пользователя
-profession_input = input(f"Выберите специальность для оценки студента {student_name}\n")
+profession_input = input(f"Выберите специальность для оценки студента {student_name}\n").title()
 
 # Проверить существование такой профессии
-professions_data_list, professions_list = load_professions()
-if profession_input.title() in professions_list:
-    profession_info = get_profession_by_title(profession_input.title())
-    check_fitness_dict = check_fitness(int(student_input), profession_input)
-    print(f"Пригодность {check_fitness_dict['fit_percent']}\n"
-          f"{student_name} знает {', '.join(check_fitness_dict['has'])}\n"
-          f"{student_name} не знает {', '.join(check_fitness_dict['lacks'])}")
-else:
-    print("У нас нет такой специальности")
-    quit()
+professions_list = load_professions(DATA_SOURCE_PROFESSIONS)
+print(professions_list, '\n', type(professions_list))
+
+for i in range(len(professions_list)):
+    if professions_list[i]["title"] == profession_input:
+        profession_info = get_profession_by_title(profession_input, DATA_SOURCE_PROFESSIONS)
+        print(profession_info, type(profession_info))
+
+#
+# for profession in professions_list:
+#     if profession["title"] == profession_input:
+#         profession_info = get_profession_by_title(profession_input, DATA_SOURCE_PROFESSIONS)
+#         print(profession_info, type(profession_info))
+
+
+        check_fitness_dict = check_fitness(student_input, profession_input)
+        print(f"Пригодность {check_fitness_dict['fit_percent']}\n"
+              f"{student_name} знает {', '.join(check_fitness_dict['has'])}\n"
+              f"{student_name} не знает {', '.join(check_fitness_dict['lacks'])}")
+        # break
+    # else:
+    #     print(profession["title"])
+    #     print(profession_input.title())
+    #     print("У нас нет такой специальности")
+    #     # quit()
