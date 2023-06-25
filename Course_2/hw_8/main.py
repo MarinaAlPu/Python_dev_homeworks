d85bc5f498a3bda73d70a8868310fc342144985f
MarinaAlPu <marina.pudovkina13@gmail.com>
2023-06-25 19:09:28 +0500

    added: call code as module

--- Course_2/hw_8/main.py
+++ Course_2/hw_8/main.py
@@ -3,22 +3,22 @@ from random import shuffle
 
 from data_questions.questions_manager import load_data, get_statistics
 
+if __name__ == "__main__":
+    DATA_SOURCE_QUESTIONS = os.path.join("data_questions/questions.json")
 
-DATA_SOURCE_QUESTIONS = os.path.join("data_questions/questions.json")
+    # Получить список вопросов
+    questions_list = load_data(DATA_SOURCE_QUESTIONS)
+    shuffle(questions_list)
+    print(questions_list)
+    # print(type(questions_list))
 
+    print("Игра начинается!")
 
-questions_list = load_data(DATA_SOURCE_QUESTIONS)
-shuffle(questions_list)
-print(questions_list)
-# print(type(questions_list))
+    # Получить ответ пользователя, прокомментировать ответ, записать ответ в соответствующее поле экземпляра класса Question
+    for question in questions_list:
+        user_answer = input(f"{question.build_question}\n")
+        question.user_answer = user_answer # в поле user_answer записываем ответ пользователя
+        print(question.build_feedback(), "\n")
 
-print("Игра начинается!")
-
-# Получить ответ пользователя, прокомментировать ответ, записать ответ в соответствующее поле экземпляра класса Question
-for question in questions_list:
-    user_answer = input(f"{question.build_question}\n")
-    question.user_answer = user_answer # в поле user_answer записываем ответ пользователя
-    print(question.build_feedback(), "\n")
-
-# Получить статистику
-print(get_statistics(questions_list))
+    # Получить статистику
+    print(get_statistics(questions_list))
