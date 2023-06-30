0fa30b9c25d5f3dbcafe497f2630d80ea9211d77
MarinaAlPu <marina.pudovkina13@gmail.com>
2023-07-01 00:27:26 +0500

    done

--- Course_2/course_work_2/main.py
+++ Course_2/course_work_2/main.py
@@ -1,31 +1,29 @@
 from utils import load_random_word
 
-# from basic_word_class import BasicWord
 from player_class import Player
 
+
 MIN_QUANTITY_OF_LETTERS = 3
+original_word_and_subwords = ""
+original_word = ""
+quantity_of_valid_words = 0
 i = 0
 used_words = []
 
-# user_name = input("Ввведите имя игрока\n")
 
-# print(f"Привет, {user_name}!")
+# Пролучить имя игрока
+user_name = input("Ввведите имя игрока\n")
 
-player = Player("PLAYER")#"(user_name)
-# print(player)
+print(f"Привет, {user_name}!")
 
+# Пролучить экземпляр класса Player
+player = Player(user_name)
 
+# Пролучить данные для игры
 original_word_and_subwords = load_random_word()
-print(original_word_and_subwords)
-
 original_word = original_word_and_subwords.original_word
-print(original_word)
-
-# quantity_of_valid_words_1 = len(original_word_and_subwords.valid_words)
-# print(quantity_of_valid_words_1)
-
 quantity_of_valid_words = original_word_and_subwords.count_valid_words()
-print(quantity_of_valid_words)
+
 
 print(f"Составьте {quantity_of_valid_words} слов из слова '{original_word}'\n"
     f"Слова должны быть не короче {MIN_QUANTITY_OF_LETTERS} букв\n"
@@ -39,38 +37,32 @@ while player.count_used_words() < quantity_of_valid_words:
     user_word = input()
 
     # Выполнить проверки
-# ## Если слово stop или стоп, прекратить игра
-# # Пользователь: стоп
-# # Программа: (выводит статистику, см шаг 6)
-# # Программа: Игра завершена, вы угадали 8 слов!
+    # Если слово stop или стоп, прекратить игру и вывести статистику
     if user_word in ["stop", "стоп"]:
         print(f"Игра завершена, вы угадали {player.count_used_words()} слов!")
         quit()
 
-    ## Если слово короче 3 букв – это неудачное слово
+    # Если слово короче 3 букв
     elif len(user_word) < MIN_QUANTITY_OF_LETTERS:
         print("слишком короткое слово")
 
-    # ## Если слова нет в списке допустимых у BasicWord – это неудачное слово
+    # Если слова нет в списке допустимых
     elif original_word_and_subwords.is_word_valid(user_word) != True:
-        # print(original_word)
-        # print()
         print("неверно")
 
-    # ## Если слово уже было угадано пользователем (Player) – это неудачное слово
+    # Если слово уже было угадано пользователем
     elif user_word in used_words:
         print("уже использовано")
 
-    # # Если все проверки пройдены, добавить слово в список использованных слов класса Player и вывести оповещение об этом пользователю
+    # Если все проверки пройдены, добавить слово в список использованных слов класса Player и вывести оповещение об этом пользователю
     else:
         used_words = player.add_new_word(user_word)
         print(used_words)
         print("верно")
+
     i += 1
 
-
-# Шаг 6
-#Вывести количество угаданных слов. Информацию получите из экземпляра класса Player.
+# Вывести количество угаданных слов
 print(f"Игра завершена, вы угадали {player.count_used_words()} слов!")
 
 quit()
